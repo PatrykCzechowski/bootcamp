6c56e6183f0362b1943c171ce6c2332e2e6bf089
PatrykCzechowski <pat.cze17@gmail.com>
2024-06-24 19:07:27 +0200

    update

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -2,5 +2,25 @@
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
     <h1>Siema</h1>
+    <br>
+    <h2>Licznik: {{ licznik }}</h2>
+  
+    <button @click="podbij">podbij</button>
+
   </main>
 </template>
+
+<script>
+export default{
+  data() {
+    return {
+      licznik: 0
+    }
+  },
+  methods: {
+    podbij(){
+      this.licznik++;
+    }
+  },
+}
+</script>
